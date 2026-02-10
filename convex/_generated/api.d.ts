9cc945a58fe645417dda9fdf84a2a8091c990aa3
dmitridrozdov <dm.drozdov@gmail.com>
2026-02-11 07:35:59 +1100

    Update api.d.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as queries from "../queries.js";
 import type * as trades from "../trades.js";
 
 import type {
@@ -17,6 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  queries: typeof queries;
   trades: typeof trades;
 }>;
 
